2baf2998937478f5cbc75f0986464ae8f37eaec8
Wediah <emmanuelwediah@gmail.com>
2022-11-29 09:50:46 +0000

    changes to home page

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import React from 'react'
 import 'reactjs-popup/dist/index.css'
 import { Element } from "react-scroll"
+import { BsFillArrowDownCircleFill } from 'react-icons/bs'
 
 
 function Homepage () {
@@ -20,9 +21,8 @@ function Homepage () {
                     </div>
                     
                     <h1 className='text-9xl text-bold text-white'>ENGEC</h1>
-                    <h4 className='text-2xl text-white'>DESIGN - CONSULTANCY - CONSTRUCTION - PROJECT MANAGEMENT.</h4>
-                    <button className='hover:transition-transform hover:scale-90 px-4 py-2 pt-3 bg-orange-500 text-white rounded h-12 w-220'>More About Us</button>
-                    <button className='hover:transition-transform hover:scale-90 px-4 py-2 pt-3 bg-white text-orange-500 rounded h-12 w-220 ml-4' >Our Services</button>
+                    <h4 className='text-2xl text-white pb-2'>DESIGN - CONSULTANCY - CONSTRUCTION - PROJECT MANAGEMENT.</h4>
+                    <button className='hover:transition-transform hover:scale-90 px-4 py-2 pt-3 bg-orange-500 text-white rounded h-12 w-220 flex'>Scroll Down For More About Us <BsFillArrowDownCircleFill className='text-3xl pl-2'/></button>
                 </div>
             </section>
 
