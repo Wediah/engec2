e91619b7132069c1ebe3e2d3813157237f2c874d
Wediah <100675006+Wediah@users.noreply.github.com>
2023-01-13 13:40:49 +0000

    adds links to button

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -4,6 +4,7 @@ import 'reactjs-popup/dist/index.css'
 import { Element } from "react-scroll"
 import { BsFillArrowDownCircleFill } from 'react-icons/bs'
 import Legon from '../public/LegonE.jpg'
+import { Link } from "react-scroll"
 
 
 
@@ -19,22 +20,22 @@ function Homepage () {
 
                     <div className='text-left  pt-80   w-screen'>
                         <h1 className=' text-7xl lg:text-9xl font-bold text-white font-serif'>ENGEC</h1>
-                        <h4 className='flex text-md text-gray-200 pb-2 font-medium font-serif'>DESIGN - CONSULTANCY - CONSTRUCTION - PROJECT MANAGEMENT.</h4>
+                        <h4 className='flex text-md text-gray-100 pb-2 font-medium font-serif'>DESIGN - CONSULTANCY - CONSTRUCTION - PROJECT MANAGEMENT.</h4>
                         
                     </div>
                     <div className='flex gap-4'>
                         <div>
-                            <button className='text-lg bg-orange-500 p-2 rounded-lg font-semibold text-white'>More About Us</button>
+                            <Link activeClass="about" to="about" smooth={true} offset={50} duration={500}><button className='text-lg bg-orange-500 p-2 rounded-lg font-semibold text-white'>More About Us</button></Link>
                         </div>
                         <div>
-                            <button className='text-lg bg-white p-2 rounded-lg font-semibold text-orange-500'>Get a Quote</button>
+                            <Link activeClass="contact" to="contact" smooth={true} offset={50} duration={500}><button className='text-lg bg-white p-2 rounded-lg font-semibold text-orange-500'>Get a Quote</button></Link>
                         </div>
                     </div>
                     <BsFillArrowDownCircleFill className='text-white text-3xl mx-auto mt-48 animate-bounce'/>
 
                 </div>
             </div>
-            
+           
 
         </Element>
     );
