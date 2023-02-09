8bd9a85010543eb3d95adfa1f51349a00d9c7ae4
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-09 19:19:32 +0000

    changes the cursor to default

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className=' text-white text-center bg-black p-2'>
+    <div className=' text-white text-center bg-black p-2 cursor-default'>
           <span className=" text-sm">COPYRIGHT © 2022  ENGEC ALL RIGHTS RESERVED</span> <br/>
           <a  className=" text-sm" href="https://myportfolio-beta-rouge.vercel.app/">Built by Emmanuel wediah</a> 
     </div>
