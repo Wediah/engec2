d3f5684d24ba1d1a434e8e622e671ba1824a5170
Wediah <100675006+Wediah@users.noreply.github.com>
2023-02-09 19:21:49 +0000

    adds hover effect to link

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className=' text-white text-center bg-black p-2 cursor-default'>
           <span className=" text-sm">COPYRIGHT © 2022  ENGEC ALL RIGHTS RESERVED</span> <br/>
-          <a  className=" text-sm" href="https://myportfolio-beta-rouge.vercel.app/">Built by Emmanuel wediah</a> 
+          <a className="hover:underline underline-offset-2 text-sm" href="https://myportfolio-beta-rouge.vercel.app/">Built by Emmanuel wediah</a> 
     </div>
   )
 }
